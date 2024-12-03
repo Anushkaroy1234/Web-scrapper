673f18463908ea4539570dd4e3089504b5043529
Anushkaroy1234 <blackpinkgargi@gmail.com>
2024-12-03 12:35:57 +0530

    Web scraping on Times of India Terrorist-Attack page

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from controller import insert_news
 from scrapper import Scrapper
 from logger_setup import set_logger
-import schedule
-import time
-
 logger=set_logger()
 
 def scrape_and_insert():
@@ -17,13 +14,8 @@ def scrape_and_insert():
             news=n.get('news')
         )
         logger.info(f"====== {index+1} DATA INSERTED ====== {res}")
-def start_scheduler():
-    schedule.every().day.at("04:00").do(scrape_and_insert)
 
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
 
 if __name__=="__main__":
     scrape_and_insert()
-    start_scheduler()
+   
